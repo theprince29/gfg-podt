76e58a94710513b56cefef0103780bf0b5c72fba
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2023-11-02 19:05:57 +0530

    02-11-2023.cpp

---
+++ Nov/02-11-2023.cpp
@@ -0,0 +1,35 @@
+// Problem Link: https://www.geeksforgeeks.org/problems/minimum-distance-between-two-numbers/1
+
+
+
+class Solution{
+  public:
+  
+    int minDist(int a[], int n, int x, int y) {
+    int minDist = INT_MAX;  // Initialize minDist to a large value
+    int xIndex = -1;        // Initialize xIndex
+    int yIndex = -1;        // Initialize yIndex
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] == x) {
+            xIndex = i;
+            if (yIndex != -1) {
+                minDist = min(minDist, abs(xIndex - yIndex));
+            }
+        } else if (a[i] == y) {
+            yIndex = i;
+            if (xIndex != -1) {
+                minDist = min(minDist, abs(yIndex - xIndex));
+            }
+        }
+    }
+
+    if (xIndex == -1 || yIndex == -1) {
+        return -1;  // Either x or y is missing in the array
+    }
+
+    return minDist;
+}
+
+
+};
