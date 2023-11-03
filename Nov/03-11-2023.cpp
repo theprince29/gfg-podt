86b8a3a7a5d29b1267005729ad505005ff44fd05
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2023-11-03 21:48:27 +0530

    Create 03-11-2023

---
+++ Nov/03-11-2023.cpp
@@ -0,0 +1,28 @@
+//problem link:https://www.geeksforgeeks.org/problems/pythagorean-triplet3018/1
+
+bool checkTriplet(int arr[], int n) 
+	{
+	    
+	    int max = INT_MIN;
+	    
+	    unordered_map<int, int> m;
+	    
+	    for(int i = 0; i<n; i++)
+	    {
+	        m[arr[i]*arr[i]] = 1;
+	    }
+	    
+	    for(int i = 0; i<n; i++)
+	    {
+	        for(int j = i+1; j<n; j++)
+	        {
+                int idx = arr[i]*arr[i];
+                
+                int idy = arr[j]*arr[j];
+	            
+	            if(m[idx+idy]) return true;
+	        }
+	    }
+	    
+	    return false;
+	}
