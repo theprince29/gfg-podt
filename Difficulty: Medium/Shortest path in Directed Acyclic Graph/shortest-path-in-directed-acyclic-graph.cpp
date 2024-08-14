541875ee396a235609909d41b831c946ceb88e15
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2024-08-14 17:16:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+private:
+    void toposort(int node, vector<pair<int,int>> adj[], vector<int>& visited, stack<int>& st) {  
+        visited[node] = 1;
+        for(auto it: adj[node]) {
+            int v = it.first;
+            if(!visited[v]) {
+                toposort(v, adj, visited, st);
+            }
+        }
+        st.push(node);
+    }    
+    
+public:
+    vector<int> shortestPath(int N, int M, vector<vector<int>>& edges) {
+        vector<pair<int,int>> adj[N];
+        
+        for(int i = 0; i < M; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+            adj[u].push_back({v, wt});
+        }
+        
+        vector<int> visited(N, 0);
+        stack<int> st;
+        
+        // Perform topological sort
+        for(int i = 0; i < N; i++) {
+            if(!visited[i]) {
+                toposort(i, adj, visited, st);
+            }
+        }
+        
+        vector<int> dist(N, INT_MAX);
+        dist[0] = 0;  // Assuming the source node is 0
+        
+        // Process nodes in topological order
+        while(!st.empty()) {
+            int node = st.top();
+            st.pop();
+            
+            if(dist[node] != INT_MAX) {
+                for(auto it: adj[node]) {
+                    int v = it.first;
+                    int wt = it.second;
+                    
+                    if(dist[node] + wt < dist[v]) {
+                        dist[v] = dist[node] + wt;
+                    }
+                }
+            }
+        }
+        
+        // Replace INT_MAX with -1 for unreachable nodes
+        for(int i = 0; i < N; i++) {
+            if(dist[i] == INT_MAX) dist[i] = -1;
+        }
+        
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        for(int i=0; i<m; ++i){
+            vector<int> temp;
+            for(int j=0; j<3; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
